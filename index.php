5bb829a1b05d6222a9bb173e036ba43c8213d43d
Nekit-Kekit <nekit.freetime@gmail.com>
2025-05-22 20:15:39 +0500

    4

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php
+s<?php
+    session_start();
+    $user_id = $_SESSION["user_id"] ?? false;
     require "vendor/autoload.php";
 
     $db = new Photos\DB();
@@ -10,47 +12,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Практика 12</title>
-    <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="style.css">
+
     <link rel="stylesheet" href="media.css">
 
 
 </head>
 <body>
-    <header>
-        <div class="popup">
-            <a href="#">Главная</a>
-            <a id="show_add_photo" href="#">Фото</a>
-            <a href="#">Посты</a>
-            <a href="#">Личный кабинет</a>
-        </div>
-        <div class="mobile_icon">
-            <img src="icon.png " alt="">
-        </div>
-    </header> 
+    
+    <?php include "header.php" ?>
     <h1>Галерея</h1>
     <div id="grid">
         <?php foreach($data as $photo): ?>
-            <?= (new Photos\Photo($photo["Image"], $photo["Text"]))->getHTML() ?>
+            <?= (new Photos\Photo($photo["id"], $photo["Image"], $photo["Text"]))->getHTML() ?>
         <?php endforeach; ?>
     </div>
     
-    <div id="add_new_photo">
-
-        <div>
-            <input id="new_photo_src" type="text" placeholder="Картинка">
-            <input id="new_photo_text" type="text" placeholder="Подпись">
-            <button id="add_photo">Добавить</button>
-            <button id="cancel">Отмена</button>
-            
-        </div>
-
-        
-    </div>
+    <?php include "add_form.php";?>
 
     <div id="popup_photo">
         <img src="" alt="">
     </div>
 
-    <script src="script.js"></script>
+    <script src="js.js"></script>
 </body>
 </html>
--- pract3/Photo.php
+++ pract3/Photo.php
@@ -1,21 +1,19 @@
 <?php
 namespace Photos;
 class Photo{
-    private $id;
     private $image;
     private $text;
 
-    public function __construct($id, $image, $text){
-        $this->id = $id;
+    public function __construct($image, $text){
         $this->image = $image;
         $this->text = $text;
     }
     
     public function getHTML(){
-        return  "<a href='image.php?id=$this->id' class='photo'>".
+        return  "<div class='photo'>".
                     "<img src='$this->image' alt=''>".
                     "<p>$this->text</p>".
-                "</a>";
+                "</div>";
     }
 }
 ?>
